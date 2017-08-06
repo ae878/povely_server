ab3a9ec0f510cb0c11a2d0b52d3e6009468b0292
ae878 <ae878@naver.com>
2017-08-06 23:11:54 +0900

    올리기 & list

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ var storage = multer.diskStorage({
      cb(null, file.uploadedFile.name + '.' + file.uploadedFile.ext);
   }
 });
-var upload = multer({storage: storage});
+var upload = multer({dest: 'uploads/'});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
