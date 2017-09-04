d081b7cffd64054ab4779eb89b98430d2dd110d0
ae878 <ae878@naver.com>
2017-09-04 20:20:10 +0900

    유저 모델 수정

--- models/user.js
+++ models/user.js
@@ -2,10 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-   userid : String,
-    name: String,
-    password: String,
-    mobile : String
+   email : String,
+    password: String
 });
 
 module.exports = mongoose.model('user', userSchema);
